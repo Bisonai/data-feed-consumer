b90d001a46b6c9a6f1681276ff668ec3993e8ed8
Martin Kersner <martin@bisonai.com>
2023-03-30 19:08:30 +0900

    feat: Update baobab data feeds

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -53,7 +53,17 @@ const config: HardhatUserConfig = {
       default: 0
     },
     aggregatorProxy: {
-      baobab: '0x1697c78D7F29eDeaf5B12DA6316d771E56A34c1e' // ETH/USD
+      baobab: '0x6492009c469373972710744eD34725D96D8c07B3' // BTC-USDT
+      // baobab: '0xFD91E50218a5451a88D7C83Ed7b555F20aa216f2' // ETH-USDT
+      // baobab: '0x1BFf2A4B141a18532A141Ec079FbAb615bba907f' // KLAY-USDT
+      // baobab: '0xCe0BBfA49C0b82B9768DFB8d1f1efC907a496842' // MATIC-USDT
+      // baobab: '0x900350a321c12Ad5388DE96087FdCF90f7ec319B' // SOL-USDT
+      // baobab: '0xFd5fb8a27ADd2Faa62Ef3c5f0EA78AEAbE1E07A3' // USDC-USDT
+      // baobab: '0xC0B2da601400c9dd49D8eF29E47a16a47932331e' // DAI-USDT
+      // baobab: '0xeD2c791eae84a9845f7832110c9Cd7E1D9670235' // DOT-USDT
+      // baobab: '0x694b6591bA06Ea48b9A07dB78B93cCdF5d144f38' // BNB-USDT
+      // baobab: '0x9ED2D63D6af73b416E0a47B56899ddE8435d89a6' // TRX-USDT
+      // baobab: '0x88DaE047193444aba53B316f40961528c326080d' // BUSD-USDT
     }
   }
 }
